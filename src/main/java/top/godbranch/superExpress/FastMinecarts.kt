dc458e48f3c68fa5a474b7787a52eb7de4da5bd3
0x002500 <tiger_pan@outlook.com>
2024-06-01 09:33:52 +0800

    [feat] remove the display the speed because of a big bug

--- src/main/java/top/godbranch/superExpress/FastMinecarts.kt
+++ src/main/java/top/godbranch/superExpress/FastMinecarts.kt
@@ -1,7 +1,5 @@
 package top.godbranch.superExpress
 
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.entity.Minecart
@@ -11,7 +9,6 @@ import org.bukkit.event.Listener
 import org.bukkit.event.vehicle.VehicleExitEvent
 import org.bukkit.event.vehicle.VehicleMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.scheduler.BukkitRunnable
 
 class FastMinecarts : JavaPlugin(), Listener {
     private val vanillaMaxSpeed = 0.4
@@ -20,7 +17,6 @@ class FastMinecarts : JavaPlugin(), Listener {
         Material.RAIL, Material.POWERED_RAIL,
         Material.DETECTOR_RAIL, Material.ACTIVATOR_RAIL
     )
-    private val playerActionBarTasks = mutableMapOf<Player, BukkitRunnable>()
 
     override fun onEnable() {
         saveDefaultConfig()
@@ -53,36 +49,13 @@ class FastMinecarts : JavaPlugin(), Listener {
         val blockBelow = railBlock.getRelative(0, -1, 0)
         val blockMultiplier = _blockMaxSpeeds[blockBelow.type] ?: vanillaMaxSpeed
 
-        // Update minecart speed on the main thread
+        // Use Folia's region scheduler to update minecart speed on the main thread
         FoliaScheduler.callSyncMethod(this) {
             minecart.maxSpeed = blockMultiplier
         }
-        val velocity = minecart.velocity
-        // Send long-term action bar message
-        displayMinecartsSpeed(minecart.passengers.first() as Player, "Speed: ${velocity.length()}", 100) // Display for 5 seconds
     }
 
-    private fun displayMinecartsSpeed(player: Player, message: String, durationTicks: Long) {
-        playerActionBarTasks[player]?.cancel()
 
-        val runnable = object : BukkitRunnable() {
-            var ticksLeft = durationTicks
-
-            override fun run() {
-                if (ticksLeft <= 0) {
-                    cancel()
-                    playerActionBarTasks.remove(player)
-                    return
-                }
-
-                player.sendActionBar(Component.text(message, NamedTextColor.YELLOW))
-                ticksLeft -= 20
-            }
-        }
-
-        runnable.runTaskTimer(this, 0, 20)
-        playerActionBarTasks[player] = runnable
-    }
 
     @EventHandler(ignoreCancelled = true)
     fun onVehicleExit(event: VehicleExitEvent) {
@@ -91,7 +64,7 @@ class FastMinecarts : JavaPlugin(), Listener {
 
         val minecart = event.vehicle as Minecart
         if (minecart.maxSpeed > vanillaMaxSpeed) {
-            // Reset minecart speed on the main thread
+            // Use Folia's region scheduler to reset minecart speed on the main thread
             FoliaScheduler.callSyncMethod(this) {
                 minecart.maxSpeed = vanillaMaxSpeed
             }
@@ -99,7 +72,6 @@ class FastMinecarts : JavaPlugin(), Listener {
     }
 
     object FoliaScheduler {
-        // Ensure tasks are executed on the main thread
         fun callSyncMethod(plugin: JavaPlugin, task: () -> Unit) {
             if (Bukkit.isPrimaryThread()) {
                 task()
@@ -109,4 +81,3 @@ class FastMinecarts : JavaPlugin(), Listener {
         }
     }
 }
-
